205aef5171d5c581e17a8dbea6fe7dd4dbcb3e7f
Etienne Gaudrain <egaudrain@gmail.com>
2020-06-04 16:28:24 +0200

    Moved externalized function `ramp` to common tools.

--- src/vt_server_common_tools.py
+++ src/vt_server_common_tools.py
@@ -17,3 +17,38 @@ def clipping_prevention(x):
         s = .98/m
         x = x*s
     return x, s
+
+def ramp(x, fs, dur, shape='cosine'):
+    """
+    The underlying function to the `"ramp"` processing module.
+
+    :param x: The input sound.
+
+    :param fs: The sampling frequency.
+
+    :param dur: The duration of the ramps (a two-element iterable).
+
+    :param shape: The shape of the ramp ('cosine' or 'linear'). Defaults to 'cosine'.
+
+    :return: The ramped sound.
+    """
+
+    if dur[0] != 0:
+        n = int(fs*dur[0])
+        w = np.linspace(0,1,n)
+        if shape=='cosine':
+            w = (1-np.cos(w*np.pi))/2
+        if len(x.shape)>1:
+            w = np.tile(w, (1,x.shape[1]))
+        x[0:n] = x[0:n] * w
+
+    if dur[1] != 0:
+        n = int(fs*dur[1])
+        w = np.linspace(1,0,n)
+        if shape=='cosine':
+            w = (1-np.cos(w*np.pi))/2
+        if len(x.shape)>1:
+            w = np.tile(w, (1,x.shape[1]))
+        x[-n:] = x[-n:] * w
+
+    return x
